5985a9d3437ed7888ebbf9c7f93a2e90c2d97bfc
Poornima <poornima.aras@gmail.com>
2025-07-31 13:30:04 +1200

    chore: initialize Spring Boot project with Gradle, base dependencies, and setup GitHub repo with .gitignore and README

---
+++ src/test/java/com/acme/air/flight_booking_service/FlightBookingServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.acme.air.flight_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
